489444e9e2caa78629dd9e82c9dc4c93d84d2650
ywonp94 <ywonp94@gmail.com>
2025-04-16 10:23:21 +0900

    Init Project

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,30 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		WebServer webServer = factory.getWebServer(servletContext ->
+				servletContext.addServlet("hello", new HttpServlet() {
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+						resp.setStatus(200);
+						resp.setHeader("Content-Type", "text/plain");
+						resp.getWriter().print("Hello Servlet");
+					}
+				}).addMapping("/hello")
+		);
+
+		webServer.start();
+	}
+}
