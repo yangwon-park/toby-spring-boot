1d52309ca245a2dbef4b240978baf11dc546e3c9
yangwon-park <ywonp94@gmail.com>
2025-04-16 16:56:15 +0900

    :necktie: DI까지 공부

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,16 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Objects;
 
-@RestController
+// SimpleHelloService 의존함
 public class HelloController {
-	@GetMapping("/hello")
+	private final HelloService helloService;
+
+	public HelloController(HelloService helloService) {
+		this.helloService = helloService;
+	}
+
 	public String hello(String name) {
-		return "Hello " + name;
+ 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,10 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,17 +17,33 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		GenericApplicationContext	 context = new GenericApplicationContext();
+		context.registerBean(HelloController.class);
+		context.registerBean(SimpleHelloService.class);
+		context.refresh(); // BeanObject 만들어줌 (초기화 작업)
+
 		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		WebServer webServer = factory.getWebServer(servletContext ->
-				servletContext.addServlet("hello", new HttpServlet() {
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						resp.setStatus(200);
-						resp.setHeader("Content-Type", "text/plain");
-						resp.getWriter().print("Hello Servlet");
-					}
-				}).addMapping("/hello")
-		);
+		WebServer webServer = factory.getWebServer(servletContext -> {
+
+					servletContext.addServlet("hello", new HttpServlet() {
+						@Override
+						protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+							// 인증, 보안, 다국어, 공통 기능 => FrontController가 담당 (Mapping, Binding 담당)
+							// 실질적인 Application 로직은 다른 Object가 담당하게 해야함
+							if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+								String name = req.getParameter("name");
+
+								HelloController helloController = context.getBean(HelloController.class);
+								String ret = helloController.hello(name);
+
+								resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+								resp.getWriter().print(ret);
+							} else {
+								resp.setStatus(HttpStatus.NOT_FOUND.value());
+							}
+						}
+					}).addMapping("/hello");
+				});
 
 		webServer.start();
 	}
