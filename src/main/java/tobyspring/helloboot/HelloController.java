3a1c846c65fdd3f57ecb61decabc6eee8fe407e9
yangwon-park <ywonp94@gmail.com>
2025-04-18 11:09:10 +0900

    :necktie: SpringBootApplication.run 과 같은 기능을 만들어냄

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,6 @@
 package tobyspring.helloboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,16 @@ import java.util.Objects;
 public class HelloController {
 	private final HelloService helloService;
 
-	public HelloController(HelloService helloService) {
+	// ApplicationContext, BeanFactory, ResourceLoader, ApplicationEventPublisher 등 몇 가지 타입에 대해
+	// “특수 의존성”(Resolvable Dependency) 으로 미리 등록해 두기 때문에, ApplicationContext는 초기화가 필요없음
+	public HelloController(HelloService helloService, ApplicationContext applicationContext) {
 		this.helloService = helloService;
+
+		System.out.println("applicationContext = " + applicationContext);
 	}
 
 	@GetMapping("/hello")
 	public String hello(String name) {
- 		return helloService.sayHello(Objects.requireNonNull(name));
+		return helloService.sayHello(Objects.requireNonNull(name));
 	}
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,35 +1,26 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan // 해당 Class 부터 그 하위 패키지까지 Component Scan 동작 -> 등록할 Bean이 너무 많아지면 등록되는 Bean을 찾아보는게 힘듦
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet(); // ApplicationContextAware를 구현한 클래스 => 스프링에 Bean으로 등록됨 => 컨테이너에 등록된 후 인터페이스이 setter 메소드를 이용해서 자동 주입해줌
+	}
+
 	public static void main(String[] args) {
-		// Spring Container를 만드는 작업
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				//  Servlet Container를 코드로 실행하면서 Servlet을 등록하는 작업
-				ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-				WebServer webServer = factory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this) // Bean을 다 뒤져서 웹 요청을 처리할 수 있 GetMapping 같은게 붙어있으면 요청 정보들을 알아서 추출해줌
-					).addMapping("/*");
-				});
-
-				webServer.start();
-			}
-		};
-
-		applicationContext.register(HellobootApplication.class); // 구성 정보를 갖고 있는 Class 자체를 등록해줌
-		applicationContext.refresh(); // BeanObject 만들어줌 (초기화 작업)
+		MySpringApplication.run(HellobootApplication.class, args);
 	}
 }
