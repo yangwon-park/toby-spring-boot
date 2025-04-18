98efa9fc22d66ab67bc8591e69e30a0499358ea1
yangwon-park <ywonp94@gmail.com>
2025-04-18 10:24:51 +0900

    :necktie: Servlet Container를 만들고 Servlet을 초기화하는 등의 작업을, Spring Container가 초기화되는 과정 중에 일어나도록 변경

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,21 +9,25 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext context = new GenericWebApplicationContext();
-		context.registerBean(HelloController.class);
-		context.registerBean(SimpleHelloService.class);
-		context.refresh(); // BeanObject 만들어줌 (초기화 작업)
+		// Spring Container를 만드는 작업
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
 
-		// --- 위 : Spring Container를 만드는 작업 ---
-		// --- 아래 : Servlet Container를 코드로 실행하면서 Servlet을 등록하는 작업---
+				//  Servlet Container를 코드로 실행하면서 Servlet을 등록하는 작업
+				ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+				WebServer webServer = factory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this) // Bean을 다 뒤져서 웹 요청을 처리할 수 있 GetMapping 같은게 붙어있으면 요청 정보들을 알아서 추출해줌
+					).addMapping("/*");
+				});
 
-		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		WebServer webServer = factory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(context) // Bean을 다 뒤져서 웹 요청을 처리할 수 있 GetMapping 같은게 붙어있으면 요청 정보들을 알아서 추출해줌
-			).addMapping("/*");
-		});
+				webServer.start();
+			}
+		};
 
-		webServer.start();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.refresh(); // BeanObject 만들어줌 (초기화 작업)
 	}
 }
